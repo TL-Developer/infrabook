4351f907a4815eb347860c501d48cddfe89a8b25
tiago.lima <tiago.lima>
2017-01-20 18:57:41 -0200

    build heroku

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 var http = require('http')
   , app  = require('./config/express')();
 
-console.log(process.env.NODE_ENV)
-
 require('./config/database.js')('mongodb://tiago:admin@ds117889.mlab.com:17889/infrabook');
+
 // if(process.env.NODE_ENV == 'production'){
+//   require('./config/database.js')('mongodb://tiago:admin@ds117889.mlab.com:17889/infrabook');
 // }else{
 //   require('./config/database.js')('mongodb://localhost/infrabook');
 // }
