77b287f6462c2e03bf26ec5f33beacdfa40971bd
tiago.lima <tiago.lima>
2017-01-23 12:52:28 -0200

    build heroku

--- config/auth.js
+++ config/auth.js
@@ -2,6 +2,6 @@ module.exports = {
   'googleAuth': {
     'clientID': '386979630805-0dn6q99co4q9uobe98homjvqnoul1bes.apps.googleusercontent.com',
     'clientSecret': 'P8iRZ6tcuI55E2eg5h9H2HzL',
-    'callbackURL': 'http://localhost:8080/auth/google/callback'
+    'callbackURL': 'https://infrabook.herokuapp.com/auth/google/callback'
   }
 };
