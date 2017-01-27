d4fd1d0c92daef07c96f940bf1ae73418b186a96
tiago.lima <tiago.lima>
2017-01-27 18:54:15 -0200

    inserindo dropzone

--- config/auth.js
+++ config/auth.js
@@ -2,7 +2,8 @@ module.exports = {
   'googleAuth': {
     'clientID': '386979630805-0dn6q99co4q9uobe98homjvqnoul1bes.apps.googleusercontent.com',
     'clientSecret': 'WmdY6Z8nFzOPljFhUN0zNTXY',
-    'callbackURL': 'http://localhost:8080/auth/google/callback'
+    'callbackURL': 'https://infrabook.herokuapp.com/auth/google/callback'
   }
 };
 // https://infrabook.herokuapp.com/auth/google/callback
+// http://localhost:8080/auth/google/callback
