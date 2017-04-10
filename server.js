400fc053f35988e9685172c86859158ff76deec2
Tiago Lima <tiago.jlima@yahoo.com.br>
2017-04-10 14:12:48 +0000

    mongodb

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 var http = require('http')
   , app  = require('./config/express')();
 
-
+/*
 if(process.env.NODE_ENV == 'production'){
   require('./config/database.js')('mongodb://tiago:admin@ds117889.mlab.com:17889/infrabook');
 }else{
-  require('./config/database.js')('mongodb://localhost/infrabook');
+  require('./config/database.js')('mongodb://localhost/infrabook')ree
 }
+*/
+
+require('./config/database.js')('mongodb://localhost/infrabook');
+
 
 http.createServer(app).listen(app.get('port'), () => {
   console.log('Server listening at port ' + app.get('port'));
